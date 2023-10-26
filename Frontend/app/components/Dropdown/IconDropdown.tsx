ed6a476256803c15f98e2d5739368e870d22f744
Saba Khubashvili <khubashvili.saba12@gmail.com>
2023-10-26 11:30:26 +0400

    feat: added iconDropdown

---
+++ Frontend/app/components/Dropdown/IconDropdown.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+
+interface Props {
+  svg: React.ReactNode;
+  content: {
+    label: string;
+    onClick: () => void;
+  }[];
+}
+
+export const IconDropdown = ({ svg, content }: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const DropdownRef = useRef<HTMLDivElement>(null)
+
+    useEffect(()=>{
+        if(isOpen){
+            const handleOutsideClick = (e:MouseEvent) =>{
+                if(DropdownRef.current && !DropdownRef.current.contains(e.target as Node)){
+                    setIsOpen(false)
+                }
+            }
+            window.addEventListener('click',handleOutsideClick)
+            return () => window.removeEventListener('click',handleOutsideClick)
+        }
+    },[isOpen])
+
+  return (
+    <div className="relative" ref={DropdownRef}>
+      <div
+        className="select-none cursor-pointer"
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+      >
+        {svg}
+      </div>
+      {isOpen && (
+        <div className="py-[10px] px-[20px] bg-white rounded-[7px] absolute -right-[20px] flex flex-col gap-[5px]">
+          {content.map((cont) => (
+            <div className="text-black text-[14px] min-w-[90px] text-left" onClick={cont.onClick} key={cont.label}>
+              {cont.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
--- Frontend/app/components/admin/pageTop/PageTop.tsx
+++ Frontend/app/components/admin/pageTop/PageTop.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import React from "react";
 import { Icon } from "../../Icon";
 import { WebsiteIcons } from "@/public/Svg/IconsObject";
 import Image from "next/image";
 import { PublicSans } from "../../assets/Fonts";
+import { IconDropdown } from "../../Dropdown/IconDropdown";
 
 export const PageTop = ({pageTitle}:{pageTitle:string}) => {
   return (
@@ -11,18 +14,26 @@ export const PageTop = ({pageTitle}:{pageTitle:string}) => {
         {pageTitle}
       </h1>
       <div className="flex gap-[24px] items-center">
-        <div className="relative cursor-pointer">
-          <Icon svg={WebsiteIcons["Bell"]} />
-          <div className="w-fit h-fit">
-            <Icon
-              svg={WebsiteIcons["RedDot"]}
-              className="absolute -right-2 -top-2 "
-            />
-            <p className="text-[#FFF]  absolute -right-[3px] -top-[5px] text-[13px] font-semibold leading-[14px]">
-              2
-            </p>
-          </div>
-        </div>
+          <IconDropdown
+            svg={
+              <div className="relative cursor-pointer">
+                <Icon svg={WebsiteIcons["Bell"]} />
+              <div className="w-fit h-fit">
+                <Icon
+                  svg={WebsiteIcons["RedDot"]}
+                  className="absolute -right-2 -top-2 "
+                />
+                <p className="text-[#FFF]  absolute -right-[3px] -top-[5px] text-[13px] font-semibold leading-[14px]">
+                  2
+                </p>
+              </div>
+            </div>
+            }
+            content={[{
+              label:'something',
+              onClick:()=>console.log('click')
+            }]}
+          />
         <div>
           <Image
             src={"/Images/User/Avatar_Placeholder.png"}
