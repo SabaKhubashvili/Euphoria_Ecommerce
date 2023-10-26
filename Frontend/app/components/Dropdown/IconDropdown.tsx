952475a876b66105512d415e3b5aaf472e85188b
Saba Khubashvili <khubashvili.saba12@gmail.com>
2023-10-26 19:00:45 +0400

    feat: added custom Row functinoality on iconDropdown

--- Frontend/app/components/Dropdown/IconDropdown.tsx
+++ Frontend/app/components/Dropdown/IconDropdown.tsx
@@ -1,15 +1,20 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
+import { RowProps } from "../admin/pageTop/PageTop";
+
+
 
 interface Props {
   svg: React.ReactNode;
   content: {
     label: string;
     onClick: () => void;
+    type: 'sold' | 'notification' | 'registration';
   }[];
+  Row: React.ComponentType<RowProps>;
 }
 
-export const IconDropdown = ({ svg, content }: Props) => {
+export const IconDropdown = ({ svg, content,Row }: Props) => {
     const [isOpen, setIsOpen] = useState(false);
     const DropdownRef = useRef<HTMLDivElement>(null)
 
@@ -37,10 +42,10 @@ export const IconDropdown = ({ svg, content }: Props) => {
       </div>
       {isOpen && (
         <div className="py-[10px] px-[20px] bg-white rounded-[7px] absolute -right-[20px] flex flex-col gap-[5px]">
-          {content.map((cont) => (
-            <div className="text-black text-[14px] min-w-[90px] text-left" onClick={cont.onClick} key={cont.label}>
-              {cont.label}
-            </div>
+          {content.map((cont) => (     
+              <Row type={cont.type} key={cont.label}>
+                {cont.label}
+              </Row>
           ))}
         </div>
       )}
--- Frontend/app/components/admin/pageTop/PageTop.tsx
+++ Frontend/app/components/admin/pageTop/PageTop.tsx
@@ -7,7 +7,29 @@ import Image from "next/image";
 import { PublicSans } from "../../assets/Fonts";
 import { IconDropdown } from "../../Dropdown/IconDropdown";
 
+
+
+export interface RowProps {
+  type: 'sold' | 'notification' | 'registration';
+  children: React.ReactNode;
+}
+
 export const PageTop = ({pageTitle}:{pageTitle:string}) => {
+
+
+  const Row = ({ type, children }:RowProps) => {
+    return (
+      <div className="min-w-[600px] flex justify-between items-center bg-[#FAFAFA] px-[5px] py-[15px]">
+        <h3 className="basis-1/2">
+          {children}
+        </h3>
+        <div className="bg-green flex justify-end p-3 rounded-[20px]">
+            <Icon svg={WebsiteIcons['Bell']} className=" w-[25px] h-[25px] fill-white stroke-white  "/>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={`flex justify-between pt-[28px] ${PublicSans.className}`}>
       <h1 className="font-bold text-[24px] leading-[22px] flex-grow">
@@ -15,6 +37,7 @@ export const PageTop = ({pageTitle}:{pageTitle:string}) => {
       </h1>
       <div className="flex gap-[24px] items-center">
           <IconDropdown
+            Row={Row}
             svg={
               <div className="relative cursor-pointer">
                 <Icon svg={WebsiteIcons["Bell"]} />
@@ -29,10 +52,60 @@ export const PageTop = ({pageTitle}:{pageTitle:string}) => {
               </div>
             </div>
             }
-            content={[{
-              label:'something',
-              onClick:()=>console.log('click')
-            }]}
+            content={
+              [
+                {
+                  label: "New user registration: Welcome Alice to our community!",
+                  onClick: () => console.log('click'),
+                  type: 'registration',
+                },
+                {
+                  label: "Sold: David's vintage record found a new home",
+                  onClick: () => console.log('click'),
+                  type: 'sold',
+                },
+                {
+                  label: "Flash sale: Save 50% on electronics today!",
+                  onClick: () => console.log('click'),
+                  type: 'notification',
+                },
+                {
+                  label: "Limited-time offer: 25% off on all clothing items",
+                  onClick: () => console.log('click'),
+                  type: 'notification',
+                },
+                {
+                  label: "Customer review: ★★★★★ Fantastic product!",
+                  onClick: () => console.log('click'),
+                  type: 'notification',
+                },
+                {
+                  label: "Registration alert: Mia just joined the community",
+                  onClick: () => console.log('click'),
+                  type: 'registration',
+                },
+                {
+                  label: "Sold: Liam's rare comic book has a new owner",
+                  onClick: () => console.log('click'),
+                  type: 'sold',
+                },
+                {
+                  label: "Notification: Explore our latest tech gadgets and deals",
+                  onClick: () => console.log('click'),
+                  type: 'notification',
+                },
+                {
+                  label: "Limited stock alert: Hurry, don't miss out on these hot deals!",
+                  onClick: () => console.log('click'),
+                  type: 'notification',
+                },
+                {
+                  label: "New arrivals: Stylish fashion collection now available",
+                  onClick: () => console.log('click'),
+                  type: 'notification',
+                },
+              ]              
+            }
           />
         <div>
           <Image
