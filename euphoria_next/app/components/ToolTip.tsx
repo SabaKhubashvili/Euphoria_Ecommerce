676f0439c288456f30aacf89bf7c611f9d961480
Saba Khubashvili <khubashvili.saba12@gmail.com>
2023-11-05 13:35:12 +0400

    feat: added format text

--- euphoria_next/app/components/ToolTip.tsx
+++ euphoria_next/app/components/ToolTip.tsx
@@ -20,7 +20,7 @@ export const ToolTip = ({children,tooltipText}:Props) => {
     <div className='relative' onMouseEnter={()=>{setIsHovering(true)}} onMouseLeave={handleMouseLeave}>
         {children}
         <div className={`absolute bottom-[1.25rem] left-[1.25rem] w-fit ${isHovering ? 'opacity-100' : 'opacity-0'} transition-opacity duration-200
-        bg-black text-white rounded-[4px] p-[10px] text-[10px] w-[125px]
+        bg-black text-white rounded-[4px] p-[10px] text-[10px] w-[145px] select-none
         `}>
             {tooltipText}
         </div>
