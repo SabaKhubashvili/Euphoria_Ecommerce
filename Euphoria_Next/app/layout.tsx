02128b7a457320a6c0286405c33232e27dea9ecd
Saba Khubashvili <khubashvili.saba12@gmail.com>
2023-12-19 16:09:14 +0400

    test: testing fixing error

--- Euphoria_Next/app/layout.tsx
+++ Euphoria_Next/app/layout.tsx
@@ -7,23 +7,23 @@ import BaseUrl from "./RestClient/ApiUrls";
 import { cookies } from "next/headers";
 import jwt_decode from 'jwt-decode';
 
-const serverSideIsAuthenticated = () => {
-  const token = cookies().get('accessToken')?.value;
-  if (!token) {
-    return false;
-  }
+// const serverSideIsAuthenticated = () => {
+//   const token = cookies().get('accessToken')?.value;
+//   if (!token) {
+//     return false;
+//   }
 
-  try {
-    const decoded = jwt_decode(token) as any;
-    if (decoded.exp < Date.now() / 1000) {
-      return false;
-    }
+//   try {
+//     const decoded = jwt_decode(token) as any;
+//     if (decoded.exp < Date.now() / 1000) {
+//       return false;
+//     }
 
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
+//     return true;
+//   } catch (error) {
+//     return false;
+//   }
+// };
 
 
 export default async function RootLayout({
@@ -33,7 +33,7 @@ export default async function RootLayout({
 }) {
   let cartData: CartInterface | null;
   
-  if (serverSideIsAuthenticated()) {
+  if (false) {
     const { data }: { data: CartInterface } = await RestClient.GetRequest(
       BaseUrl.getCart,
       cookies().get("accessToken")?.value
