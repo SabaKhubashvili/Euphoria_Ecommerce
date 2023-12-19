41cb2e07c4fccdddff197046a3a6bfc47f211c32
Saba Khubashvili <khubashvili.saba12@gmail.com>
2023-12-19 16:28:19 +0400

    feat: added server side autherization

--- Euphoria_Next/app/layout.tsx
+++ Euphoria_Next/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  cookies.
+  cookies().set('accessToken','');
   let cartData: CartInterface | null;
   
   if (false) {
